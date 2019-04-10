97e3bf9dfb131cf400778abbbc87b50ed7c37fea
jean-marcfiaschi <jm.fiaschi@gmail.com>
2019-04-10 10:31:46 +0200

    Create project.

---
+++ README.md
@@ -0,0 +1,13 @@
+# phumbor-cli
+
+Little tools in order to forge thumbor url on the fly.
+
+## How to install
+```
+$ docker-compose run --rm composer install --prefer-dist --no-interaction --ignore-platform-reqs --optimize-autoloader -vvv --no-scripts
+```
+
+## How to use
+```
+$ docker-compose run --rm php bin/console
+```
---
+++ app/src/Command/ImageBuilderCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Command;
+
+use Jb\Bundle\PhumborBundle\Transformer\BaseTransformer;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command to generate url image
+ */
+class ImageBuilderCommand extends Command implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    protected static $defaultName = 'phumbor-cli:image:get-url';
+
+    /**
+     * @var BaseTransformer
+     */
+    protected $baseTransformer;
+
+    /**
+     * @param BaseTransformer $baseTransformer
+     */
+    public function __construct(BaseTransformer $baseTransformer)
+    {
+        $this->baseTransformer = $baseTransformer;
+        $this->logger          = new NullLogger();
+
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Get signed image url.')
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to build signed image url with phumbor.')
+            ->addArgument('image', InputArgument::REQUIRED, 'Build the image url with this image.')
+            ->addOption(
+                'transformation',
+                't',
+                InputOption::VALUE_OPTIONAL,
+                'Transformation to apply on the image.',
+                'default'
+            );
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|void|null
+     *
+     * @throws \Jb\Bundle\PhumborBundle\Transformer\Exception\UnknownTransformationException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
+    {
+        $output->writeln(
+            $this->baseTransformer->transform($input->getArgument('image'), $input->getOption('transformation'))
+        );
+    }
+}
